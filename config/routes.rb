cfece4b766e90536764945e69b0ec8d24824b187
Benoit Liabastre <b.liabastre@gmail.com>
2013-10-17 06:08:55 +0200

    fixed home sign up btn

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ LocateMyStickers::Application.routes.draw do
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/signout', to: 'sessions#destroy'#, via: :delete
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
