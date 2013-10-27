4f9d4b6e109499ec822112713c9d3fd00858a351
Benoit Liabastre <b.liabastre@gmail.com>
2013-10-27 07:16:44 +0100

    Lots of stuff

--- config/initializers/pousse.rb
+++ config/initializers/pousse.rb
@@ -2,10 +2,7 @@ Pousse::configure do |config|
   config.server = "http://pousse-lms.herokuapp.com:10092/" # Warning: You must specify the port in development mode (eg: http://mypousssette.herokuapp.com:80)
   config.secret = "f6d25e25316f21b0e23d7d22844330e7"
   if ENV['REDISTOGO_URL'].present?
-    # uri = URI.parse(ENV['REDISTOGO_URL'])
-    # config.redis = { host: uri.host, port: uri.port, password: uri.password }
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    Resque.redis = REDIS
+    uri = URI.parse(ENV['REDISTOGO_URL'])
+    config.redis = { host: uri.host, port: uri.port, password: uri.password }
   end
 end
