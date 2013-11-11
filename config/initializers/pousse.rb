4ed3602ea4e0fe7c732e7bb99ab71e37eb2c15eb
Benoit Liabastre <b.liabastre@gmail.com>
2013-11-11 22:13:16 +0100

    pusher + pack user data

--- config/initializers/pousse.rb
+++
@@ -1,8 +0,0 @@
-Pousse::configure do |config|
-  config.server = ENV['POUSSETTE_SERVER'] # Warning: You must specify the port in development mode (eg: http://mypousssette.herokuapp.com:80)
-  config.secret = ENV['POUSSE_SECRET']
-  if ENV['REDISTOGO_URL'].present?
-    uri = URI.parse(ENV['REDISTOGO_URL'])
-    config.redis = { host: uri.host, port: uri.port, password: uri.password }
-  end
-end
